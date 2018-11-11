a5c37c1cbf99f8fb58da1f539b5631290e436443
VIXR0 <remixxgraphics@gmail.com>
2018-11-11 15:15:12 -0500

    Updated cmd

--- cmd/slots.js
+++ cmd/slots.js
@@ -1,14 +1,26 @@
-const slots = ['🍇', '🍊', '🍐', '🍒', '🍋'];
+const slots = ['🍊', '🍒', '🍋'];
 
-exports.run = (client, message, args) => {
+exports.run = (client, message, args, Discord) => {
     const slotOne = slots[Math.floor(Math.random() * slots.length)];
     const slotTwo = slots[Math.floor(Math.random() * slots.length)];
     const slotThree = slots[Math.floor(Math.random() * slots.length)];
 
+    let winEmbed = new Discord.RichEmbed()
+    .setColor('0xffffff')
+    .setTitle('~ Win ~')
+    .setDescription('Congratulations!')
+    .addField("Slot Outcome", `${slotOne} | ${slotTwo} | ${slotThree}`, true);
+
+    let loseEmbed = new Discord.RichEmbed()
+    .setColor('0xffffff')
+    .setTitle('~ Loss ~')
+    .setDescription('Better Luck Next Time!')
+    .addField("Slot Outcome", `${slotOne} | ${slotTwo} | ${slotThree}`, true);
+
     if (slotOne == slotTwo && slotOne == slotThree) {
-        message.channel.send(`\`${slotOne} | ${slotTwo} | ${slotThree}\nCongrats! You Won!\``);
+        message.channel.send(winEmbed);
     } else {
-        message.channel.send(`\`${slotOne} | ${slotTwo} | ${slotThree}\nAww, Better Luck Next Time!\``);
+        message.channel.send(loseEmbed);
     }
 
 }
