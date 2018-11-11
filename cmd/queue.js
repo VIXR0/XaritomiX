cb20a0e8f260c56aaa24b18656b1ae31c28cff03
Brandon Stephen <xaritomix@zerenegaming.net>
2018-11-11 20:15:22 +0000

    Still updating Queue

--- cmd/queue.js
+++ cmd/queue.js
@@ -4,17 +4,29 @@ exports.run = (client, message, args, Discord) => {
     const serverQueue = client.music.get(message.guild.id).songs;
     let index = 1, page = 1, totalPages;
 
-    totalPages = 1;
+    (serverQueue.length > 10) ?  totalPages = serverQueue.length / 10 : totalPages = 1;
+    
+}
+function chunkArray(myArray, chunk_size){
+    var index = 0;
+    var arrayLength = myArray.length;
+    var tempArray = [];
+
+    for (index = 0; index < arrayLength; index += chunk_size) {
+        myChunk = myArray.slice(index, index+chunk_size);
+        tempArray.push(myChunk);
+    }
+
+    return tempArray;
+}
+
+function displayEmbed(message, songs, options, Discord) {
     let embed = new Discord.RichEmbed() 
     .setTitle(`Server Queue For Server: ${message.guild.name}`)
     .setDescription(`${index++} - ${serverQueue.map(songs => songs.title)}`)
     .setFooter(`You are on page: ${page} / ${totalPages}`)
 
     message.channel.send(embed);
-}
-function queueChunk() {
-
-    
 }
 
 exports.conf = {
