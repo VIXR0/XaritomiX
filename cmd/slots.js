404f7eb53cfccf8c22dc017ec7270d4bf321562f
VIXR0 <remixxgraphics@gmail.com>
2018-11-11 14:53:48 -0500

    Added slots.js

---
+++ cmd/slots.js
@@ -0,0 +1,27 @@
+const slots = ['🍇', '🍊', '🍐', '🍒', '🍋'];
+
+exports.run = (client, message, args) => {
+    const slotOne = slots[Math.floor(Math.random() * slots.length)];
+    const slotTwo = slots[Math.floor(Math.random() * slots.length)];
+    const slotThree = slots[Math.floor(Math.random() * slots.length)];
+
+    if (slotOne == slotTwo && slotOne == slotThree) {
+        message.channel.send(`${slotOne} | ${slotTwo} | ${slotThree}\nCongrats! You Won!`);
+    } else {
+        message.channel.send(`${slotOne} | ${slotTwo} | ${slotThree}\nAww, Better Luck Next Time!`);
+    }
+
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+};
+  
+exports.help = {
+    name: "slots",
+    description: "This command runs a slots simulation",
+    usage: "!slots"
+};
