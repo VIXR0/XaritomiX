c130c02f7721c0fe3c82ba0bab0cfed31e5e9f6c
Brandon Stephen <xaritomix@zerenegaming.net>
2018-11-12 22:31:51 +0000

    Updated to have per server configerations

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
 const dfsettings = require("./dfsettings.json");
+const guildSettingsDB = require("./models/Guild_settings");
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/XaritomiX', { useNewUrlParser: true});
 
 client.music = new Map();
 client.commands = new Discord.Collection();
@@ -23,49 +26,69 @@ fs.readdir("./cmd/", (err, files) => {
   console.log("==========================================")
 });
 
-fs.readdir("./db/", (err, files) => {
-    if (err) console.error(err);
-    console.log("============= | DB Configs | =============");
-    console.log(`Loading a total of ${files.length} db files`);
-    files.forEach(f => {
-        let propsTwo = require(`./db/${f}`);
-        console.log(`Loading file ${propsTwo.help.name}. All Good`);
-        client.db.set(propsTwo.help.name, propsTwo);
-    });
-    console.log("==========================================")
-});
-
 client.on("ready", () => {
     console.log("Ready!!");
 });
 
 client.on("guildCreate", guild => {
-    client.db.get("Discord_Guilds").run(client, guild, "", "create");
+    addGuildSetting(guild);
 });
 
 client.on("message", message => {
-    let guild = message.guild;
+    let prefix;
     if (message.guild === null) return;
-    if (!message.content.startsWith(dfsettings.prefix)) return;
-    let command = message.content.split(" ")[0].slice(dfsettings.prefix.length).toLowerCase();
-    let args = message.content.split(" ").slice(1);
-    // let perms = client.permissions(message);
-    let cmd;
-    if (client.commands.has(command)) {
-        cmd = client.commands.get(command);
-    } else if (client.aliases.has(command)) {
-        cmd = client.commands.get(client.aliases.get(command));
-    }
-    if (cmd) {
-        // if (perms < cmd.conf.permLevel) return message.channel.send(`Im sorry, but this command requires a permission level of ${cmd.conf.permLevel} or higher.`);
-    cmd.run(client, message, args, Discord);
-    }
+
+    guildSettingsDB.findOne({ Server_ID: message.guild.id}, (err, guild) => {
+        if (err) return; 
+        if (!guild) {
+            addGuildSetting(message.guild);
+        } else {
+            let prefix = guild.Server_Prefix;
+            if (!message.content.startsWith(prefix)) return;
+            let command = message.content.split(" ")[0].slice(prefix.length).toLowerCase();
+            let args = message.content.split(" ").slice(1);
+            // let perms = client.permissions(message);
+            let cmd;
+            if (client.commands.has(command)) {
+                cmd = client.commands.get(command);
+            } else if (client.aliases.has(command)) {
+                cmd = client.commands.get(client.aliases.get(command));
+            }
+            if (cmd) {
+                // if (perms < cmd.conf.permLevel) return message.channel.send(`Im sorry, but this command requires a permission level of ${cmd.conf.permLevel} or higher.`);
+            cmd.run(client, message, args, Discord);
+            }
+        }
+    });
+
 });
 
 client.isVoiceChannel = function(message) {
     const voiceChannel = message.member.voiceChannel;
     if (!voiceChannel) return message.channel.send("I'm sorry but you must be in the voice channel to request a song");
     return voiceChannel;
+}
+
+function addGuildSetting(guild) {
+    guildSettingsDB.findOne({ Server_ID: guild.id}, (err, guildSettings) => {
+        if (err) return;
+        if (guildSettings) return; 
+
+        const newGuild = new guildSettingsDB({
+            Server_Name: guild.name,
+            Server_ID: guild.id,
+            Server_Mod_Channel: "ModLogs",
+            Server_Prefix: "!",
+            Server_Moderator_Role: "Moderators",
+            Server_Admin_Role: "Administrator",
+            Server_Member_Role: "Members",
+            Server_Mute_Role: "Muted",
+            Server_Default_Volume: "100", 
+            Server_Play_Next_Song_Message: true
+        });
+
+        newGuild.save().catch(err => console.log(err));
+    });
 }
 
 
---
+++ models/Guild_settings.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const guildSettings = mongoose.Schema({
+    Server_Name: String,
+    Server_ID: String,
+    Server_Mod_Channel: String,
+    Server_Prefix: String,
+    Server_Moderator_Role: String,
+    Server_Admin_Role: String,
+    Server_Member_Role: String,
+    Server_Mute_Role: String,
+    Server_Default_Volume: Number, 
+    Server_Play_Next_Song_Message: Boolean,
+    Date: { type: Date, default: Date.now() }
+});
+
+module.exports = mongoose.model("Playlists", guildSettings);
