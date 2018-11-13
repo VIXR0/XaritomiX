f5b4729763514dd20e3a78a1a98dfd9a8879b3f5
Brandon Stephen <xaritomix@zerenegaming.net>
2018-11-13 04:39:51 +0000

    Added db functionality

--- cmd/mute.js
+++ cmd/mute.js
@@ -1,40 +1,50 @@
 const ms = require("ms");
+const settings = require("../models/Guild_settings");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/XaritomiX", { useNewUrlParser: true});
 
 exports.run = async (client, message, args, Discord) => {
-    let toMute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    let muteRole = message.guild.roles.find(role=> role.name === "Muted");
 
-    if(!toMute) return message.channel.send('`You must specify a user!`');
-    if(toMute.hasPermission("KICK_MEMBERS")) return message.channel.send('`I can\'t mute that member!`');
+    settings.findOne({Server_ID: message.guild.id}, async (err, result) => {
+        if (err) return;
+        if (!result) return;
 
-    if(!muteRole){
-        try{
-            muteRole = await message.guild.createRole({
-                name: "Muted",
-                color: "0xffffff",
-                permissions:[]
-            })
-            message.guild.channels.forEach(async (channel, id) => {
-                await channel.overwritePermissions(muteRole, {
-                    SEND_MESSAGES: false,
-                    ADD_REACTIONS: false
+        let toMute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        let muteRole = message.guild.roles.find(role=> role.name === result.Server_Mute_Role);
+
+        if(!toMute) return message.channel.send('`You must specify a user!`');
+        if(toMute.hasPermission("KICK_MEMBERS")) return message.channel.send('`I can\'t mute that member!`');
+
+        if(!muteRole){
+            try{
+                muteRole = await message.guild.createRole({
+                    name: result.Server_Mute_Role,
+                    color: "0xffffff",
+                    permissions:[]
+                })
+                message.guild.channels.forEach(async (channel, id) => {
+                    await channel.overwritePermissions(muteRole, {
+                        SEND_MESSAGES: false,
+                        ADD_REACTIONS: false
+                    });
                 });
-            });
-        } catch(e) {
-            console.log(e.stack)
+            } catch(e) {
+                console.log(e.stack)
+            }
         }
-    }
 
-    let muteTime = args[1];
-    if(!muteTime) return message.channel.send('`You didn\'t specify a time!`');
+        let muteTime = args[1];
+        if(!muteTime) return message.channel.send('`You didn\'t specify a time!`');
 
-    await(toMute.addRole(muteRole.id));
-    message.channel.send(`<@${toMute.id}> has been muted for ${ms(ms(muteTime))}`);
+        await(toMute.addRole(muteRole.id));
+        message.channel.send(`<@${toMute.id}> has been muted for ${ms(ms(muteTime))}`);
 
-    setTimeout(function() {
-        toMute.removeRole(muteRole.id);
-        message.channel.send(`<@${toMute.id}> has been unmuted.`);
-    }, ms(muteTime));
+        setTimeout(function() {
+            toMute.removeRole(muteRole.id);
+            message.channel.send(`<@${toMute.id}> has been unmuted.`);
+        }, ms(muteTime));
+    });
+    
 
 }
 
